0991c753da16289f59d19b32cc8d58c680e537d9
klolapc\klola <ariantony@gmail.com>
2017-05-06 11:09:16 +0700

    Menambahkan tombol ke link external (Google.com)

--- app/src/main/java/com/example/klola/imsohungry/MainActivity.java
+++ app/src/main/java/com/example/klola/imsohungry/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.klola.imsohungry;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,14 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void eatCookie (View view) {
-        ImageView beforeCookie = (ImageView) findViewById(R.id.android_cookie_image_view);
-        beforeCookie.setImageResource(R.drawable.cookie);
-        beforeCookie.set
-
-        TextView status = (TextView) findViewById(R.id.test);
-        status.setText("I am soooo full...");
+        Button intentBtn = (Button) findViewById(R.id.btnIntent);
+        intentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("test","haloooooowwww");
+                Intent i= new Intent(MainActivity.this, Main2Activity.class);
+                i.putExtra("Value1", "Contoh data 1");
+                i.putExtra("var1", "var1Oke");
+                startActivity(i);
+            }
+        });
+     Button intentUrlBtn = (Button) findViewById(R.id.intentUrlBtn);
+        intentUrlBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url="http://www.google.com";
+                Intent i= new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
     }
 }
